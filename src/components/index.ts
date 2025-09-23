34b00ca38ab6449e12211fe4aac03f379bf4b9c6
Mainak Karmakar <iammainak58@gmail.com>
2025-09-23 10:57:16 +0530

    initialize project

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import { Footer } from "./footer";
+import { ExperienceSection } from "./experience-section";
+import { ExpertSection } from "./expert-section";
+import { FAQSection } from "./faq-section";
+import { Header } from "./header";
+import { HeroSection } from "./hero-section";
+import { JobUpdatesSection } from "./job-updates-section";
+import { WhatWeDoSection } from "./what-we-do-section";
+import { ThemeProvider } from "./theme-provider";
+import { ServicesSection } from "./services-section";
+
+export {
+  ExperienceSection,
+  ExpertSection,
+  FAQSection,
+  Footer,
+  Header,
+  HeroSection,
+  JobUpdatesSection,
+  WhatWeDoSection,
+  ThemeProvider,
+  ServicesSection,
+};
